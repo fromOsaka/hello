89b17d6bca082ebdd8edd3d97405effcfdee33ff
osaka <daniilarakelyan@MacBook-Pro-Daniil.local>
2023-04-01 18:13:02 +0300

    im make a turtle code

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print('Hello git!')
+from turtle import Turtle
+r = Turtle()
+m = 30 
+for i in range(4):
+    r.forward(5*m)
+    r.right(90)
+r.penup()
+for x in range(0,10):
+    for y in range(-10,10):
+        r.goto(x*m,y*m)
+        r.dot(2,'red')
