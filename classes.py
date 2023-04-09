4f28fd2b44c24aef707ff65b78ee1d97b74d48a1
osaka <sensh0god@gmail.com>
2023-04-10 00:12:10 +0300

    Add a flight class, with some def's incde

--- classes.py
+++ classes.py
@@ -4,6 +4,28 @@ class Point():
         self.y = y
 
 p = Point(2,8)
-print(p.x)
-print(p.y)
-        
+#print(p.x)
+#print(p.y)
+
+class Flight():
+    def __init__(self,capacity):
+        self.capacity = capacity
+        self.passengers = []
+
+    def add_passenger(self,name):
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)
+        return True
+
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+guys = ["Harry", "Mary", "Ron",'Andrian']
+flight = Flight(2)
+for person in guys:
+    secces = flight.add_passenger(person)
+    if secces:
+        print(f'We added {person} to Flight')
+    else:
+        print(f"Sorry, mr/ms {person}, not today")
